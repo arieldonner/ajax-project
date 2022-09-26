eca23eeb9a0ec543c9542e812322d5a9c059e3f8
arieldonner <arieldonner@gmail.com>
2022-09-26 23:28:50 +0000

    Started working on search bar to find games

---
+++ js/main.js
@@ -0,0 +1,115 @@
+var gameList = [];
+/* Search Bar for Games */
+var $search = document.querySelector('#search');
+
+var $searchButton = document.querySelector('.button-search');
+$searchButton.addEventListener('click', function (event) {
+  // console.log($search.value);
+
+  var targetUrl = encodeURIComponent('https://steamcommunity.com/actions/SearchApps/' + $search.value);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
+  xhr.setRequestHeader('token', 'abc123');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // console.log(xhr.response);
+    for (var j = 0; j < gameList.length; j++) {
+      gameList[j].remove();
+    }
+    for (var i = 0; i < xhr.response.length; i++) {
+      var appId = xhr.response[i].appid;
+
+      var targetUrl2 = encodeURIComponent('https://store.steampowered.com/api/appdetails?appids=' + appId);
+
+      var xhr2 = new XMLHttpRequest();
+      xhr2.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl2);
+      xhr2.setRequestHeader('token', 'abc123');
+      xhr2.responseType = 'json';
+      xhr2.addEventListener('load', function () {
+        // console.log(xhr2.response);
+      });
+      xhr2.send();
+
+      createEntry(xhr.response[i]);
+      // var $description = document.querySelector('description');
+      // $description.textContent = xhr2.response[appId].data.short_description;
+    }
+  });
+  xhr.send();
+});
+
+var $ul = document.querySelector('ul');
+/* Create Game Entry with Dom */
+function createEntry(entry) {
+
+  var list = document.createElement('li');
+  list.className = 'game';
+
+  var card = document.createElement('div');
+  card.className = 'card';
+  var cardContainer = list.appendChild(card);
+
+  var col2 = document.createElement('div');
+  col2.className = 'column-two-fifths';
+  var col2div = cardContainer.appendChild(col2);
+
+  var img = document.createElement('img');
+  img.setAttribute('src', entry.logo);
+  img.setAttribute('alt', 'image for the game');
+  col2div.appendChild(img);
+
+  var col3 = document.createElement('div');
+  col3.className = 'column-three-fifths card-text';
+  var col3div = cardContainer.appendChild(col3);
+
+  var titleDiv = document.createElement('div');
+  titleDiv.className = 'card-title';
+  var titleDivContainer = col3div.appendChild(titleDiv);
+
+  var gameTitle = document.createElement('h2');
+  gameTitle.className = 'game-title';
+  gameTitle.textContent = entry.name;
+  titleDivContainer.appendChild(gameTitle);
+
+  var heart = document.createElement('i');
+  heart.className = 'fa-regular fa-heart';
+  titleDivContainer.appendChild(heart);
+
+  var description = document.createElement('p');
+  description.className = 'description';
+  // description.textContent = appid[idNum].data.short_description;
+  col3.appendChild(description);
+
+  var release = document.createElement('p');
+  // release.textContent = ;
+  col3.appendChild(release);
+
+  var genre = document.createElement('p');
+  // genre.textContent = ;
+  col3.appendChild(genre);
+
+  $ul.appendChild(list);
+
+  gameList.push(list);
+}
+
+/* <li class="game">
+  <div class="card">
+    <div class="column-two-fifths">
+      <img
+        src="https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/en_US/games/switch/r/rune-factory-5-switch/hero"
+        alt="game image">
+    </div>
+    <div class="column-three-fifths card-text">
+      <div class="card-title">
+        <h2 class="game-title">Rune Factory 5</h2>
+        <i class="fa-regular fa-heart"></i>
+      </div>
+      <p>Description: As the newest ranger of a peacekeeping organization known as SEED, protect your community by rounding up rowdy monsters
+        and going on special missions. Cultivate your farm and friendships alike while unravelling rune-related mysteries!</p>
+      <p>Release Date: Jul 13, 2022</p>
+      <p>Genre: Adventure, RPG, Simulation</p>
+    </div>
+  </div>
+</li> */
