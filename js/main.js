4704559327d11b775b566d0d8b306421080b170c
arieldonner <arieldonner@gmail.com>
2022-10-05 16:29:50 +0000

    Added html form for adding a new game to MyCodex

--- js/main.js
+++ js/main.js
@@ -168,6 +168,7 @@ var $gamesContainer = document.querySelector('.games-container');
 var $codexContainer = document.querySelector('.codex-container');
 var $notesContainer = document.querySelector('.notes-container');
 var $editContainer = document.querySelector('.edit-container');
+var $addGameContainer = document.querySelector('.addGame-container');
 
 function handleView(view) {
   data.view = view;
@@ -177,30 +178,42 @@ function handleView(view) {
     $codexContainer.className = 'container codex-container hidden';
     $notesContainer.className = 'container notes-container hidden';
     $editContainer.className = 'container edit-container hidden';
+    $addGameContainer.className = 'container addGame-container hidden';
   } else if (view === 'featured') {
     $featuredContainer.className = 'container featured-container';
     $gamesContainer.className = 'container games-container hidden';
     $codexContainer.className = 'container codex-container hidden';
     $notesContainer.className = 'container notes-container hidden';
     $editContainer.className = 'container edit-container hidden';
+    $addGameContainer.className = 'container addGame-container hidden';
   } else if (view === 'codex') {
     $featuredContainer.className = 'container featured-container hidden';
     $gamesContainer.className = 'container games-container hidden';
     $codexContainer.className = 'container codex-container';
     $notesContainer.className = 'container notes-container hidden';
     $editContainer.className = 'container edit-container hidden';
+    $addGameContainer.className = 'container addGame-container hidden';
   } else if (view === 'notes') {
     $featuredContainer.className = 'container featured-container hidden';
     $gamesContainer.className = 'container games-container hidden';
     $codexContainer.className = 'container codex-container hidden';
     $notesContainer.className = 'container notes-container';
     $editContainer.className = 'container edit-container hidden';
+    $addGameContainer.className = 'container addGame-container hidden';
   } else if (view === 'edit') {
     $featuredContainer.className = 'container featured-container hidden';
     $gamesContainer.className = 'container games-container hidden';
     $codexContainer.className = 'container codex-container hidden';
     $notesContainer.className = 'container notes-container hidden';
     $editContainer.className = 'container edit-container';
+    $addGameContainer.className = 'container addGame-container hidden';
+  } else if (view === 'addGame') {
+    $featuredContainer.className = 'container featured-container hidden';
+    $gamesContainer.className = 'container games-container hidden';
+    $codexContainer.className = 'container codex-container hidden';
+    $notesContainer.className = 'container notes-container hidden';
+    $editContainer.className = 'container edit-container hidden';
+    $addGameContainer.className = 'container addGame-container';
   }
 }
 
@@ -729,8 +742,11 @@ $deleteConfirm.addEventListener('click', function (event) {
   }
 });
 
+var $addButton = document.querySelector('.add-button');
+$addButton.addEventListener('click', function (event) {
+  handleView('addGame');
+});
+
 /*
-Deleted game hearts need to go back to unfilled in featured games
 Need to add play status to MyCodex
-Need to add buttons to MyCodex
 */
